7eb73da06544faa6f3d0ccb9fd5c2c651353cad9
kuriakose mathew <a00258755@student.ait.ie>
2019-03-07 12:45:36 +0000

    Front end with display product and add product functionality

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+    new CopyWebpackPlugin([{ from: "./src/list-item.html", to: "list-item.html" }])
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
 };
